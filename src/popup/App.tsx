6751909ae2e09ce6caad450134cdc0a312b3bdaf
maooson <maooson@qq.com>
2023-02-19 21:22:40 +0800

    Update popup page & Increase GPT ratelimit

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,11 +1,9 @@
-import { GlobeIcon, LinkExternalIcon } from '@primer/octicons-react'
+import { GlobeIcon, GearIcon, CheckCircleFillIcon } from '@primer/octicons-react'
 import { useCallback } from 'react'
 import useSWR from 'swr'
 import Browser from 'webextension-polyfill'
 import '../base.css'
 import logo from '../logo.png'
-
-const isChrome = /chrome/i.test(navigator.userAgent)
 
 function App() {
   const accessTokenQuery = useSWR(
@@ -13,18 +11,9 @@ function App() {
     () => Browser.runtime.sendMessage({ type: 'GET_ACCESS_TOKEN' }),
     { shouldRetryOnError: false },
   )
-  const hideShortcutsTipQuery = useSWR('hideShortcutsTip', async () => {
-    const { hideShortcutsTip } = await Browser.storage.local.get('hideShortcutsTip')
-    return !!hideShortcutsTip
-  })
 
-  const openLandingPage = useCallback(() => {
-    Browser.tabs.create({ url: 'https://chatgpt4wechat.aow.me' })
-  }, [])
-
-  const openShortcutsPage = useCallback(() => {
-    Browser.storage.local.set({ hideShortcutsTip: true })
-    Browser.tabs.create({ url: 'chrome://extensions/shortcuts' })
+  const openOptionsPage = useCallback(() => {
+    Browser.runtime.sendMessage({ type: 'OPEN_OPTIONS_PAGE' })
   }, [])
 
   return (
@@ -33,19 +22,19 @@ function App() {
         <img src={logo} className="w-5 h-5 rounded-sm" />
         <p className="text-sm font-semibold m-0 ml-1">ChatGPT for Wechat</p>
         <div className="grow"></div>
-        <span className="cursor-pointer leading-[0]" onClick={openLandingPage}>
-          <LinkExternalIcon size={16} />
+        <span className="cursor-pointer leading-[0]" onClick={openOptionsPage}>
+          <GearIcon size={16} />
         </span>
       </div>
-      {isChrome && !hideShortcutsTipQuery.isLoading && !hideShortcutsTipQuery.data && (
-        <p className="m-0 mb-2">
-          Tip:{' '}
-          <a onClick={openShortcutsPage} className="underline cursor-pointer">
-            setup shortcuts
-          </a>{' '}
-          for faster access.
-        </p>
-      )}
+      <div className="hero bg-gradient-to-b from-purple-500 to-pink-500">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <p className="mb-5 text-base">开发不易，请我喝杯咖啡吧，感谢您的支持！</p>
+            <img className="w-48 h-48" src={'https://models.aoq.me/zansm.jpg'} alt="赞赏码" />
+          </div>
+        </div>
+      </div>
       {(() => {
         if (accessTokenQuery.isLoading) {
           return (
@@ -55,19 +44,45 @@ function App() {
           )
         }
         if (accessTokenQuery.data) {
-          return <iframe src="https://chat.openai.com" className="grow border-none" />
+          return (
+            <div className="grow justify-center items-center flex flex-col">
+              <p className="text-base text-center">
+                ChatGPT 登录状态正常<br />
+                <CheckCircleFillIcon className="mt-4 fill-green-500" size={48} />
+              </p>
+              <iframe src="https://chat.openai.com/chat" className="w-0 h-0 grow-0 border-none" />
+            </div>
+          )
         }
         return (
-          <div className="grow flex flex-col justify-center">
-            <p className="text-base px-2 text-center">
-              Please login and pass Cloudflare check at{' '}
-              <a href="https://chat.openai.com" target="_blank" rel="noreferrer">
-                chat.openai.com
-              </a>
-            </p>
+          <div className="grow flex flex-col">
+            <div className="grid place-items-center">
+              <p className="text-base text-center">请检查 ChatGPT 页面是否已登录 {' '}
+                <a href="https://chat.openai.com" target="_blank" rel="noreferrer">
+                  chat.openai.com
+                </a><br />
+                若已经登录，请尝试再次点击插件图标或关闭代理
+              </p>
+            </div>
+            <div className="divider my-0 text-base">或</div>
+            <div className="grid place-items-center">
+              <p className="text-base">打开 <span className="cursor-pointer leading-[0]" onClick={openOptionsPage}>
+                <GearIcon className="mr-2" size={16} />插件配置页面</span>，切换 AI 源</p>
+            </div>
           </div>
         )
       })()}
+      <footer className="footer sticky bottom-0 left-0 right-0 w-auto p-4 bg-neutral text-white">
+        <div className="w-full place-items-center">
+          <span className="footer-title mt-2">
+            &copy; 2023{' '}
+            <a className="text-white" href="https://aow.me" target="_blank" rel="noreferrer">
+              aow.me
+            </a>{' '}
+            版权所有 <a className="text-white" href="https://aow.me/disclaimer" target="_blank">免责声明</a>
+          </span>
+        </div>
+      </footer>
     </div>
   )
 }
